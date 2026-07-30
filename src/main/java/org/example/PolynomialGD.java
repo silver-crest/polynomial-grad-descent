4adbb8223451c6a11f3834f92636c315fd8a2942
shamrocksprings10 <217861583+silver-crest@users.noreply.github.com>
2026-07-30 18:03:51 -0500

    accessor and modifier methods for coeffs

--- src/main/java/org/example/PolynomialGD.java
+++ src/main/java/org/example/PolynomialGD.java
@@ -7,16 +7,48 @@ import java.util.Collections;
 
 public class PolynomialGD {
     public static final double LEARNING_RATE = 0.001;
+    public static final double DELTA = 0.001;
 
-    private final ArrayList<Integer> coefficients;
+    private final int degree;
+    private final ArrayList<Double> coefficients;
 
     public PolynomialGD(int degree) {
+        this.degree = degree;
         coefficients = new ArrayList<>(degree);
-        Collections.fill(coefficients, 0);
+        Collections.fill(coefficients, 0.0);
+    }
+
+    // Copy constructor
+    public PolynomialGD(PolynomialGD p) {
+        degree = p.degree;
+        coefficients = new ArrayList<>(p.coefficients);
     }
 
     public void gradientDescent(ArrayList<PVector> points) {
 
+    }
+
+    public ArrayList<Double> getGradient() {
+        for (int d = 0; d <= degree; d++) {
+            var copy = new PolynomialGD(this);
+            copy.
+        }
+    }
+
+    public int getDegree() {
+        return degree;
+    }
+
+    public double getCoeff(int degree) {
+        return coefficients.get(degree);
+    }
+
+    public void updateCoeff(int degree, double newCoeff) {
+        coefficients.set(degree, newCoeff);
+    }
+
+    public void offsetCoeff(int degree, double offset) {
+        updateCoeff(degree, getCoeff(degree) + offset);
     }
 
 }
