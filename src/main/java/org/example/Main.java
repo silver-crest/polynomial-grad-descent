46845eaac0aed1fcf8c860a31c35d8b24ca73045
shamrocksprings10 <217861583+silver-crest@users.noreply.github.com>
2026-07-30 17:36:05 -0500

    Processing has a window now

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import processing.core.PApplet;
+
 public class Main {
     public static void main(String[] args) {
-
+        PApplet.main("org.example.Simulation");
     }
 }
--- src/main/java/org/example/Simulation.java
+++ src/main/java/org/example/Simulation.java
@@ -1,4 +1,20 @@
 package org.example;
 
-public class Simulation {
+import processing.core.PApplet;
+
+public class Simulation extends PApplet {
+    @Override
+    public void settings() {
+        size(800, 800);
+    }
+
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public void draw() {
+        super.draw();
+    }
 }
